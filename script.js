6e3c4c9ce4527d109668c4e3c73efe48c493012c
navatejvel <veldhandi.navatej@gmail.com>
2026-04-13 19:59:09 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,30 @@
-et score = 0;
+let score = 0;
 
 function suggestFood() {
 let food = document.getElementById("foodInput").value.toLowerCase();
 let result = "";
 let impact = "";
+let score = 0;
 
 if (food.includes("burger")) {
 result = "Try a homemade veggie burger 🌱";
 impact = "You can save ~100L water and reduce carbon footprint 🌍";
-score += 10;
+score = 10;
 }
 else if (food.includes("pizza")) {
 result = "Try fresh homemade pizza with vegetables 🍕";
 impact = "Fresh ingredients reduce packaging waste ♻️";
-score += 8;
+score = 8;
 }
 else if (food.includes("chips")) {
 result = "Try fruits or baked snacks 🍎";
 impact = "Less processed food = healthier + eco-friendly 🌿";
-score += 7;
+score = 7;
 }
 else {
 result = "Choose fresh, organic food 🌿";
 impact = "Supports healthy soil and sustainable farming 🌱";
-score += 5;
+score = 5;
 }
 
 document.getElementById("result").innerHTML = result + "<br>" + impact;
